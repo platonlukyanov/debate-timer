aa8c9637952924a85088aa47ddef45057f0cfa98
Platon Lukyanov <platonioluka@gmail.com>
2025-02-06 22:28:30 +0300

    Linting set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 function App() {
-
-  return (
-     <h1 className="text-center w-full">Hello world!</h1> 
-  )
+	return (
+		<h1 className="text-center w-full">
+      		Hello world!
+		</h1> 
+	)
 }
 
 export default App
